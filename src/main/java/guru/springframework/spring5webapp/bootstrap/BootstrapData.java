251cc56acab9833d235b6654884e53d4ab9ed866
PatForAll <siekmeier.p@gmail.com>
2021-06-15 20:59:06 +0200

    add publisher relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -25,31 +25,39 @@ public class BootstrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Publisher penguin = new Publisher("Penguin", "Penguin Way 1", "Penguin City", "12345");
+        publisherRepository.save(penguin);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "12345");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(penguin);
+        penguin.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(penguin);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development", "67890");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(penguin);
+        penguin.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(penguin);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Authors: " + authorRepository.count());
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("First Author-ID: " + eric.getId());
-
-        Publisher penguin = new Publisher("Penguin Roundhouse", "Penguin Way 1", "Penguina", "12345");
-        publisherRepository.save(penguin);
-
         System.out.println("Number of publishers: " + publisherRepository.count());
+        System.out.println("Number of books for 1st publisher: " + penguin.getBooks().size());
 
     }
 }
